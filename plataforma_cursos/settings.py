c4fb58d1acb15fcd2303320f0207efc2901b031d
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2021-10-08 15:46:11 -0300

    Criação das funcionalidades de avaliação e comentário das aulas

--- plataforma_cursos/settings.py
+++ plataforma_cursos/settings.py
@@ -66,6 +66,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+            'libraries':{
+            'my_templatetag': 'cursos.templatetags.filtros',
+            
+            }
         },
     },
 ]
